e1521fb62f14ae0f308812f710d1fd5905764674
sanechka <belikalexandra@gmail.com>
2020-12-22 23:40:36 +0300

    wip

--- src/components/ClientForm.vue
+++ src/components/ClientForm.vue
@@ -2,45 +2,67 @@
   <div class="form">
     <form>
       <div class="form-block">
-        <div class="form__group">
-          <label for="lastName">Фамилия</label>
-          <input class="form__control" id="lastName" name="lastName" v-model="$v.lastName.$model">
-          <!--<p class="error" v-if="$v.lastName.required">
-            <span>Пожалуйста, заполните данное поле</span>>
-          </p>-->
+        <div class="form-group" :class="{ 'form-group__error': $v.lastName.$error }">
+          <label class="required" for="lastName">Фамилия</label>
+          <input class="form__control" id="lastName" name="lastName" v-model="$v.lastName.$model"/>
+          <p class="error" v-if="!$v.lastName.required && $v.lastName.$dirty">
+            <span>Пожалуйста, заполните данное поле</span>
+          </p>
         </div>
-        <div class="form__group">
-          <label for="firstName">Имя</label>
-          <input class="form__control" id="firstName" name="firstName" v-model="firstName">
+        <div class="form-group" :class="{ 'form-group__error': $v.firstName.$error }">
+          <label class="required" for="firstName">Имя</label>
+          <input class="form__control" id="firstName" name="firstName" v-model="$v.firstName.$model">
+          <p class="error" v-if="!$v.firstName.required && $v.firstName.$dirty">
+            <span>Пожалуйста, заполните данное поле</span>
+          </p>
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="middleName">Отчество</label>
           <input class="form__control" id="middleName" name="middleName" v-model="middleName">
         </div>
-        <div class="form__group">
-          <label for="dateOfBirth">Дата рождения</label>
-          <input class="form__control" type="date" id="dateOfBirth" name="dateOfBirth" v-model="dateOfBirth">
+        <div class="form-group" :class="{ 'form-group__error': $v.dateOfBirth.$error }">
+          <label class="required" for="dateOfBirth">Дата рождения</label>
+          <input class="form__control" type="date" id="dateOfBirth" name="dateOfBirth" v-model="$v.dateOfBirth.$model" value=" ">
+          <p class="error" v-if="!$v.dateOfBirth.required && $v.dateOfBirth.$dirty">
+            <span>Пожалуйста, заполните данное поле</span>
+          </p>
         </div>
-        <div class="form__group">
-          <label for="phoneNumber">Номер телефона</label>
-          <input class="form__control" id="phoneNumber" name="phoneNumber" v-model="phoneNumber">
+        <div class="form-group" :class="{ 'form-group__error': $v.phoneNumber.$error }">
+          <label class="required" for="phoneNumber">Номер телефона</label>
+          <input class="form__control" id="phoneNumber" name="phoneNumber" v-model="$v.phoneNumber.$model">
+          <p class="error" v-if="!$v.phoneNumber.required && $v.phoneNumber.$dirty">
+            <span>Пожалуйста, заполните данное поле</span>
+          </p>
+          <p class="error" v-if="$v.phoneNumber.$invalid && $v.phoneNumber.$dirty">
+            <span>Пожалуйста, введите 11 цифр, начиная с 7</span>
+          </p>
         </div>
-        <div>
-          <label for="male">М</label>
+        <div class="form__checkbox">
+          <p class="label">Пол</p>
+          <label class="radio radio-gradient">
+          <span class="radio__input">
+            <input type="radio" name="radio">
+            <span class="radio__control"></span>
+          </span>
+            <span class="radio__label">Radio 1 - radial-gradient</span>
+          </label>
+          <label  for="male">м</label>
           <input type="radio" id="male" name="male" value="male" v-model="sex">
-          <label for="female">Ж</label>
-          <input type="radio" id="female" name="female" value="female" v-model="sex">
+          <label for="female">ж</label>
+          <input  type="radio" id="female" name="female" value="female" v-model="sex">
         </div>
-        <div class="form__group">
-          <label for="clientGroup">Группа клиентов</label>
-          <select class="form__control" multiple id="clientGroup" name="clientGroup" v-model="clientGroup">
-            <option value="vip">VIP</option>
-            <option value="problem">Проблемные</option>
-            <option value="oms">ОМС</option>
-          </select>
+        <div class="form__checkbox">
+          <p class="required label">Группа клиентов</p>
+
+          <input type="checkbox" id="vip" name="vip" value="VIP" v-model="clientGroup">
+          <label  for="vip">VIP</label>
+          <input  type="checkbox" id="problem" name="problem" value="Проблемные" v-model="clientGroup">
+          <label for="problem">Проблемные</label>
+          <input  type="checkbox" id="oms" name="oms" value="ОМС" v-model="clientGroup">
+          <label for="oms">ОМС</label>
         </div>
         <div>
-          <label for="doctor"Лечащий врач></label>
+          <p for="doctor">Лечащий врач</p>
           <select class="form__control" id="doctor" name="doctor" v-model="doctor">
             <option value="1">Иванов</option>
             <option value="2">Захаров</option>
@@ -53,54 +75,54 @@
         </div>
       </div>
       <div class="form-block">
-        <div class="form__group">
+        <div class="form-group">
           <label for="index">Индекс</label>
           <input class="form__control" id="index" name="index" v-model="index">
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="country">Страна</label>
           <input class="form__control" id="country" name="country" v-model="country">
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="region">Область</label>
           <input class="form__control" id="region" name="region" v-model="region">
         </div>
-        <div class="form__group">
-          <label for="city">Город</label>
+        <div class="form-group">
+          <label class="required" for="city">Город</label>
           <input class="form__control" id="city" name="city" v-model="city">
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="street">Улица</label>
           <input class="form__control" id="street" name="street" v-model="street">
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="house">Дом</label>
           <input class="form__control" id="house" name="house" v-model="house">
         </div>
       </div>
       <div class="form-block">
-        <div class="form__group">
-          <label for="documentType">Тип документа</label>
+        <div class="form-group">
+          <label class="required" for="documentType">Тип документа</label>
           <select class="form__control" name="documentType" id="documentType" v-model="documentType">
             <option value="passport">Паспорт</option>
             <option value="birthCertificate">Свидетельство о рождении</option>
             <option value="driverLicense">Водительское удостоверение</option>
           </select>
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="serial">Серия</label>
           <input class="form__control" id="serial" name="serial" v-model="serial">
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="number">Номер</label>
           <input class="form__control" id="number" name="number" v-model="number">
         </div>
-        <div class="form__group">
+        <div class="form-group">
           <label for="authority">Кем выдан</label>
           <input class="form__control" id="authority" name="authority" v-model="authority">
         </div>
-        <div class="form__group">
-          <label for="issueDate">Дата выдачи</label>
+        <div class="form-group">
+          <label class="required" for="issueDate">Дата выдачи</label>
           <input class="form__control" id="issueDate" name="issueDate" v-model="issueDate">
         </div>
 
@@ -125,7 +147,7 @@ export default {
       phoneNumber: '',
       clientGroup: [],
       doctor: '',
-      sendSms: true,
+      sendSms: false,
       index: null,
       country: '',
       region: '',
@@ -143,7 +165,18 @@ export default {
     lastName: {
       required
     },
-  }
+    firstName: {
+      required
+    },
+    dateOfBirth: {
+      required
+    },
+    phoneNumber: {
+      required,
+      validFormat: val =>  /^\+?7(\d{10})$/.test(val)
+    }
+  },
+
 }
 
 </script>
